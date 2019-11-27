9937263959e605f24948ca5a85a4d40fb5c8833a
UCVP7OF <UCVP7OF@VWVITHLPUA707.ppune.vwgi.apac.vwg>
2019-11-27 16:50:08 +0530

    Something is changes

--- src/main/java/com/example/controller/HelloController.java
+++ src/main/java/com/example/controller/HelloController.java
@@ -1,24 +1,69 @@
 package com.example.controller;
 
+import java.util.Base64;
+
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.annotations.ApiOperation;
 
 @RestController
 public class HelloController {
 
 	@GetMapping("/")
+	@ApiOperation(value = "View Home Input", response = String.class)
 	public ResponseEntity<String> getWelcomePage() {
 
 		return new ResponseEntity<String>("Welcome", HttpStatus.OK);
 	}
 
 	@GetMapping("/getUser/{input}")
+	@ApiOperation(value = "View The Input", response = String.class)
 	public ResponseEntity<String> getUser(@PathVariable String input) {
 
 		return new ResponseEntity<String>(input, HttpStatus.OK);
+	}
+
+	@GetMapping("hmac")
+	public ResponseEntity<String> getEnycription(@RequestParam String key) {
+		StringBuilder sb = new StringBuilder();
+		sb.append("PUT\n"); // method
+		sb.append('\n'); // content encoding
+		sb.append('\n'); // content language
+		sb.append("0" + '\n'); // content length
+		sb.append('\n'); // md5 (optional)
+		sb.append('\n'); // content type
+		sb.append('\n'); // legacy date
+		sb.append('\n'); // if-modified-since
+		sb.append('\n'); // if-match
+		sb.append('\n'); // if-none-match
+		sb.append('\n'); // if-unmodified-since
+		sb.append('\n'); // range
+		sb.append("x-ms-date:" + "Wed, 27 Nov 2019 14:06:03 GMT" + '\n'); // headers
+		sb.append("x-ms-version:2014-02-14\n");
+		sb.append("/" + "scodastorage" + "/mycontainer1" + "\nrestype:container");
+		String message = sb.toString();
+		try {
+			String secret = "TeX3YIaHk6YG3sYVrxKWShMLq2Nslxnqi1c1k8nNAGyi9ds59mZuoo4GoyHdPLr2DO0+HwUyeYMUDOue35up5A==";
+			byte[] decodedBytes = Base64.getDecoder().decode(secret);
+			System.out.println(decodedBytes.toString());
+			Mac sha256_HMAC = Mac.getInstance("HmacSHA256");
+			SecretKeySpec secret_key = new SecretKeySpec(decodedBytes, "HmacSHA256");
+			sha256_HMAC.init(secret_key);
+			String hash = Base64.getEncoder().encodeToString(sha256_HMAC.doFinal(message.getBytes()));
+			System.out.println(hash);
+			return new ResponseEntity<String>("welcome user " + hash, HttpStatus.OK);
+		} catch (Exception e) {
+			System.out.println("Error");
+		}
+		return new ResponseEntity<String>("welcome user", HttpStatus.OK);
 	}
 
 }
